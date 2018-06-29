6f4ed26afe3efc4be88bc2825fd8c363a6330b18
kinoli7 <kinoli@hotmail.com>
2018-06-29 19:22:04 +0200

    Online funciona con score

--- project/server-logic.js
+++ project/server-logic.js
@@ -14,44 +14,114 @@ server.listen(port, () => {
 app.use(express.static(path.join(__dirname, 'public')))
 
 const state = {
-    players: []
+    players: [],
+    bullets: []
 }
 
 // Constants
 const WORLD_WIDTH = 2000
 const WORLD_HEIGHT = 2000
 
-const speed = 3
+const playerSizeX = 53
+const playerSizeY = 40
+const playerSpeed = 6
 
+const bulletSizeRadius = 5
+const bulletSpeed = 12
+const SHOOTING_DELAY = 400 // in miliseconds
 
 function reset () {
-    const { players } = state
+    const { players, bullets } = state
 
     players.forEach(player => {
         player.x = 50 * WORLD_HEIGHT
         player.y = 50 * WORLD_WIDTH
     })
 }
-
 reset()
+
+function collision (player, bullet) {
+    const sizeSum = (player.sizex/2 + bullet.sizeRadius/2)
+    return ( (Math.abs(player.x - bullet.x) < sizeSum) &&
+    (Math.abs(player.y - bullet.y) < sizeSum) )
+}
 
 function logic () {
     setTimeout(logic, 20)
-    const { players } = state
+    const { players, bullets } = state
 
     // Player Logic
     players.forEach( player => {
         const { keyboard } = player
 
-        if (keyboard.left) player.x -= speed
-        if (keyboard.right) player.x += speed
-        if (keyboard.up) player.y -= speed
-        if (keyboard.down) player.y += speed
+        if (keyboard.left) {
+            player.x -= playerSpeed
+            player.lastDir = 'left'
+        }
+        if (keyboard.right) {
+            player.x += playerSpeed
+            player.lastDir = 'right'
+        }
+        if (keyboard.up) {
+            player.y -= playerSpeed
+            player.lastDir = 'up'
+        }
+        if (keyboard.down) {
+            player.y += playerSpeed
+            player.lastDir = 'down'
+        }
+        if (keyboard.shoot && (Date.now() - player.shooting >= SHOOTING_DELAY) ) {
+            player.shooting = Date.now()
+            const bullet = {
+                playerId: player.id,
+                x: player.x,
+                y: player.y,
+                dir: player.lastDir,
+                sizeRadius: bulletSizeRadius
+            }
+            state.bullets.push(bullet)
+        }
     })
-    
+
+    // Bullets state
+    let i = bullets.length-1
+    while (i >= 0) {
+        bullet = bullets[i]
+
+        if (bullet.dir == 'left') bullet.x -= bulletSpeed
+        if (bullet.dir == 'right') bullet.x += bulletSpeed
+        if (bullet.dir == 'up') bullet.y -= bulletSpeed
+        if (bullet.dir == 'down') bullet.y += bulletSpeed
+
+        let pi = 0, collide = false, bullet_owner
+        while (pi < players.length && !collide) {
+            bullet_owner = players.findIndex(function (player) {
+                return player.id == bullet.playerId
+            })
+            
+            if (bullet_owner != pi) {
+                collide = collision(players[pi], bullet)
+                if (collide) players[bullet_owner].score += 1
+            }
+            pi++
+        }
+
+        if (bullet.x > WORLD_WIDTH || bullet.y > WORLD_HEIGHT || collide) {
+            bullets.splice(i, 1)
+            --i
+        }
+        --i
+    }
+
+    players.forEach(player => {
+        //console.log(player.score)
+    })
+
+    //let frozenState = JSON.stringify(state)
     io.sockets.emit('state', state)
 }
 logic()
+
 
 io.on('connection', socket => {
 
@@ -60,7 +130,11 @@ io.on('connection', socket => {
         y: 0,
         lastDir: 'down',
         keyboard: {},
-        id: socket.id
+        shooting: 0,
+        sizex: playerSizeX,
+        sizey: playerSizeY,
+        id: socket.id,
+        score: 0
     }
 
     state.players.push(player)
